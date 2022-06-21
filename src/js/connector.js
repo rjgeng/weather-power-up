ba7df6a044af40c42cafd18f5a607d8b9e715a8d
RFNoC <rj.geng@gmail.com>
2022-06-20 22:51:31 -0700

    Fetch weather data for card badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,29 @@
 console.log("Hello World!");
 
-window.TrelloPowerUp.initialize({ 
-    "card-badges": function (t, opts) {
-        // return an array of card badges for the given card  
-        return t.card('all')
-            .then(function(card) {
-                console.log(card);
-                return [{
-                    text: card.idShort    
-                }];
-            })     
-    }    
-});
+window.TrelloPowerUp.initialize({
+    'card-badges': function(t, opts) {
+      // return an array of card badges for the given card
+      return t.card('coordinates')
+      .then(function(card) {
+        console.log(card);
+        if (card.coordinates) {
+          const { latitude, longitude } = card.coordinates;          
+          // our card has a location, let's fetch the current weather
+          return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`)
+          .then(function(response) {            
+            return response.json();
+          })
+          .then(function(weatherData) {            
+            return [{
+              text: weatherData.name
+            },{
+              text: weatherData.main.temp.toString()
+            }, {
+              text: weatherData.wind.speed.toString()
+            }]
+          });
+        }
+        return [];
+      })
+    }
+  });
