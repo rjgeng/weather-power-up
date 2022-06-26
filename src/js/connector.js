4e49cce17d459edaa1ea8493c28c49205db0c544
RFNoC <rj.geng@gmail.com>
2022-06-26 14:48:08 -0700

    Use dynamic badges to fetch weather every 30m

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,19 +1,16 @@
 const Promise = window.TrelloPowerUp.Promise;
-
 const fetchWeatherData = function(t) {
   return Promise.all([t.card('coordinates'), t.get('card', 'shared')])
     .spread(function(card, cache) {
       if (!card.coordinates) {
         return null;
       }
-
       const { latitude, longitude } = card.coordinates;
       const location = `${latitude}:${longitude}`;
       if (cache && cache.location === location && cache.expires >= Date.now() && cache.weatherData) {
         console.log('Cache Hit', location);
         return cache.weatherData;
       }
-
       console.log('Cache Miss', location);
       // our card has a location, let's fetch the current weather
       return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`)
@@ -34,38 +31,56 @@ const fetchWeatherData = function(t) {
       });
     });
 };
-
-const getBadgesForWeatherData = function(weatherData) {
-  if (weatherData == null) {
-    return [];
-  }
-  return [{
-    title: 'Temperature',
-    text: weatherData.main.formattedTemp,
-  }, {
-    title: 'Wind Speed',
-    text: `🌬️ ${weatherData.wind.speed} knots`,
-  }, {
-    title: 'Conditions',
-    icon: `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
-    text: weatherData.weather[0].main,
-  }]
+const getWeatherBadges = function(t) {
+  return t.card('coordinates')
+  .then(function(card) {
+    if (!card.coordinates) {
+      return [];
+    }
+    return [{
+      dynamic: function(t) {
+        return fetchWeatherData(t)
+        .then(function(weatherData) {
+          return {
+            title: 'Temperature',
+            text: weatherData.main.formattedTemp,
+            refresh: 30 * 60,
+          };
+        });
+      }
+    }, {
+      dynamic: function(t) {
+        return fetchWeatherData(t)
+        .then(function(weatherData) {
+          return {
+            title: 'Wind Speed',
+            text: `🌬️ ${weatherData.wind.speed} knots`,
+            refresh: 30 * 60,
+          };
+        });
+      }
+    }, {
+      dynamic: function(t) {
+        return fetchWeatherData(t)
+        .then(function(weatherData) {
+          return {
+            title: 'Conditions',
+            icon: `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
+            text: weatherData.weather[0].main,
+            refresh: 30 * 60,
+          };
+        });
+      }
+    }];
+  })
 };
 window.TrelloPowerUp.initialize({
   'card-badges': function(t) {
     // return an array of card badges for the given card
-    return fetchWeatherData(t)
-    .then(function(weatherData) {
-      const badges = getBadgesForWeatherData(weatherData);
-      return badges;
-    });
+    return getWeatherBadges(t);
   },
   'card-detail-badges': function(t) {
     // return an array of card badges for the given card
-    return fetchWeatherData(t)
-    .then(function(weatherData) {
-      const badges = getBadgesForWeatherData(weatherData);
-      return badges;
-    });
+    return getWeatherBadges(t);
   },
 });
